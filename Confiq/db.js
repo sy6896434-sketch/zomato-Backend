fc89e17a15ef8493b5837518172454f824c054f4
Nidhi Yadav <sy6896434@gmail.com>
2026-07-12 20:56:02 +0530

    Final fix - removed alter and using MYSQL_URL

--- Confiq/db.js
+++ Confiq/db.js
@@ -1,12 +1,19 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize(process.env.MYSQL_URL, {
-  dialect: "mysql",
-  logging: false,
-});
+const db = new Sequelize(
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 db.authenticate()
-  .then(() => console.log("✅ Database Connected"))
-  .catch((err) => console.error("❌ DB Error:", err));
+  .then(() => console.log("✅ Database Connected Successfully"))
+  .catch((err) => console.error("❌ Database Connection Failed:", err));
 
 module.exports = db;
